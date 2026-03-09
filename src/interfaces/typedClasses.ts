c636daf139e1abf3d0a9b71fcf58d232d6c393f1
René Rivas <reinaldorrivas@gmail.com>
2026-03-09 17:08:30 -0400

    fh-ts-guide: Minor change

--- src/interfaces/typedClasses.ts
+++ src/interfaces/typedClasses.ts
@@ -8,25 +8,25 @@ interface Human {
   isHuman: boolean;
 }
 
-export const typedClasses = (): void => {
-  class Mutant implements Xmen, Human {
-    public name: string;
-    public realName: string;
-    public isHuman: boolean;
+class Mutant implements Xmen, Human {
+  public name: string;
+  public realName: string;
+  public isHuman: boolean;
 
-    constructor(name: string, realName: string, isHuman: boolean) {
-      this.name = name;
-      this.realName = realName;
-      this.isHuman = isHuman;
-    }
-
-    public mutantPower(power: string): string {
-      return this.isHuman
-        ? "This Xmen is human, it doesn't have any power"
-        : `It's power is: ${power}`;
-    }
+  constructor(name: string, realName: string, isHuman: boolean) {
+    this.name = name;
+    this.realName = realName;
+    this.isHuman = isHuman;
   }
 
+  public mutantPower(power: string): string {
+    return this.isHuman
+      ? "This Xmen is human, it doesn't have any power"
+      : `It's power is: ${power}`;
+  }
+}
+
+export const typedClasses = (): void => {
   const wolverine: Mutant = new Mutant("Wolverine", "Logan", false);
 
   console.log("Wolverine", wolverine);
